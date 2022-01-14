5d42f8dbec1418c6362c369b5d2495c583c2640b
Zhikang Xu(Jacob) <50785344+Jabberwockiii@users.noreply.github.com>
2022-01-14 17:13:43 -0500

    Update app.js

--- devfolio-master/app.js
+++ devfolio-master/app.js
@@ -1,28 +1,18 @@
 var http = require('http');
 var fs = require('fs');
-var path = require('path');
 
-http.createServer(function(req, res){
+const PORT=3000; 
 
-    if(req.url === "/"){
-        fs.readFile("index.html", "UTF-8", function(err, html){
-            res.writeHead(200, {"Content-Type": "text/html"});
-            res.end(html);
-        });
-    }else if(req.url.match("\.css$")){
-        var cssPath = path.join(__dirname, 'public', req.url);
-        var fileStream = fs.createReadStream(cssPath, "UTF-8");
-        res.writeHead(200, {"Content-Type": "text/css"});
-        fileStream.pipe(res);
+fs.readFile('./index.html', function (err, html) {
 
-    }else if(req.url.match("\.png$")){
-        var imagePath = path.join(__dirname, 'public', req.url);
-        var fileStream = fs.createReadStream(imagePath);
-        res.writeHead(200, {"Content-Type": "image/png"});
-        fileStream.pipe(res);
-    }else{
-        res.writeHead(404, {"Content-Type": "text/html"});
-        res.end("No Page Found");
-    }
+    if (err) throw err;    
 
-}).listen(3000);
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(PORT);
+});
+
+
+
